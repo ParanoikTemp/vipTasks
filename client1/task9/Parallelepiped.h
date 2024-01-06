c3199f40731bad325a1408b1dc0967f33b3ff1b0
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2024-01-06 23:44:05 +0300

    Update Parallelepiped.h

--- client1/task9/Parallelepiped.h
+++ client1/task9/Parallelepiped.h
@@ -8,7 +8,15 @@ private:
     double height;
 
 public:
+    Parallelepiped(): length(0), width(0), height(0) {}
+
     Parallelepiped(double l, double w, double h);
+
+    Parallelepiped(const Parallelepiped& other) = default;
+
+    ~Parallelepiped() = default;
+
+    Parallelepiped& operator=(const Parallelepiped& other) = default;
 
     double getVolume() const;
 
