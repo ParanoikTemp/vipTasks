5a9362aca3505f096360e217058abc933c72cc69
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2024-01-06 23:44:19 +0300

    Update Ball.h

--- client1/task9/Ball.h
+++ client1/task9/Ball.h
@@ -7,9 +7,19 @@ private:
     double radius;
 
 public:
+    Ball(): radius(0) {}
+
     Ball(double r);
+
+    Ball(const Ball& other) = default;
+
+    ~Ball() = default;
+
+    Ball& operator=(const Ball& other) = default;
 
     double getVolume() const;
 
     double getSurfaceArea() const;
+
+
 };
