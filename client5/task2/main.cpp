33a2e1e4ff5b15fc9fdbc32134d9103ec075817d
Parax <64136866+ParanoikTemp@users.noreply.github.com>
2024-01-07 01:01:14 +0300

    Update main.cpp

--- client5/task2/main.cpp
+++ client5/task2/main.cpp
@@ -4,16 +4,22 @@ int main() {
     BinaryTree tree;
 
     // Добавление элементов в дерево
+    tree.insert(16);
     tree.insert(5);
     tree.insert(3);
     tree.insert(7);
     tree.insert(2);
     tree.insert(4);
-    tree.insert(6);
     tree.insert(8);
 
     // Установка значения parent
     tree.setParent();
+
+    tree.printTree();
+    std::cout << "\n\n";
+    tree.deleteElement(5);
+    tree.printTree();
+    std::cout << "\n\n";
 
     // Вывод дерева в прямом порядке
     std::cout << "In-order traversal: ";
